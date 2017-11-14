cb1fde6a898bb7565efd51b0078febd62c809be7
Unknown <albin.anner@gmail.com>
2017-11-14 22:18:36 +0100

    fixed makefile

--- Computational_physics/E1/Task1/main.c
+++ Computational_physics/E1/Task1/main.c
@@ -5,8 +5,6 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <gsl/gsl_fft_real.h>
-#include <gsl/gsl_fft_halfcomplex.h>
 #define PI 3.141592653589
 #define n 250 /*number of timesteps*/
 
